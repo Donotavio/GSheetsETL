86055c9a89bb2786542c1e373bde44bcb0d7ee14
otavi <ribeitemp@gmail.com>
2023-04-14 18:40:13 -0300

    create start system

--- main.py
+++ main.py
@@ -1,29 +1,14 @@
-import gspread
-import pandas as pd
-from oauth2client.service_account import ServiceAccountCredentials
+from workers import db_connection
+from workers import get_sheets_gs
 
-scope = [
-    "https://www.googleapis.com/auth/spreadsheets",
-    "https://www.googleapis.com/auth/drive",
-]
+table_name = input("Enter table name: ")
 
-# defines key_file.json address
-service_account_file = (
-    r"EDIT"
+data = get_sheets_gs.get_data()
+
+engine = db_connection.db_con()
+
+data.to_sql(
+    table_name, con=engine, if_exists="replace", index=False, schema="sheets"
 )
 
-creds = ServiceAccountCredentials.from_json_keyfile_name(service_account_file, scope)
-
-client = gspread.authorize(creds)
-
-# get the instance of the Spreadsheet
-sheet = client.open("EDIT")
-
-# get the first sheet of the Spreadsheet
-sheet_instance = sheet.get_worksheet(0)
-
-records_data = sheet_instance.get_all_records()
-
-records_df = pd.DataFrame.from_dict(records_data)
-
-print(records_df.head())
+print("FIM!")
